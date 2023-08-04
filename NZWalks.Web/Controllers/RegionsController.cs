e2bfdcab06ebd3aa1d3cf9429779c5b6beff64d7
musyokaeric <62944492+musyokaeric@users.noreply.github.com>
2023-08-04 17:16:17 +0300

    GET - Consume Web API
    Get all regions

---
+++ NZWalks.Web/Controllers/RegionsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NZWalks.Web.Models.DTO;
+
+namespace NZWalks.Web.Controllers
+{
+    public class RegionsController : Controller
+    {
+        private readonly IHttpClientFactory httpClientFactory;
+
+        public RegionsController(IHttpClientFactory httpClientFactory)
+        {
+            this.httpClientFactory = httpClientFactory;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            List<RegionDto> regions = new List<RegionDto>();
+            try
+            {
+                // Get all Regions from Web API
+                var client = httpClientFactory.CreateClient();
+
+                var httpResponse = await client.GetAsync("https://localhost:7001/api/regions");
+                httpResponse.EnsureSuccessStatusCode();
+
+                regions.AddRange(await httpResponse.Content.ReadFromJsonAsync<IEnumerable<RegionDto>>());
+            }
+            catch (Exception ex)
+            {
+                // Log the exception
+            }
+
+            return View(regions);
+        }
+    }
+}
